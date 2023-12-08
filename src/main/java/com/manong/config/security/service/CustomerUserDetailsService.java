71016dd66da3b8c5176ac4fcfa46dd6713f4b832
kimeray <kimerayforw@outlook.com>
2023-12-09 05:02:09 +0800

    fix: 查询用户成功未返回user信息

--- src/main/java/com/manong/config/security/service/CustomerUserDetailsService.java
+++ src/main/java/com/manong/config/security/service/CustomerUserDetailsService.java
@@ -14,18 +14,6 @@ import javax.annotation.Resource;
  */
 @Component
 public class CustomerUserDetailsService implements UserDetailsService {
-    /**
-     * Locates the user based on the username. In the actual implementation, the search
-     * may possibly be case sensitive, or case insensitive depending on how the
-     * implementation instance is configured. In this case, the <code>UserDetails</code>
-     * object that comes back may have a username that is of a different case than what
-     * was actually requested..
-     *
-     * @param username the username identifying the user whose data is required.
-     * @return a fully populated user record (never <code>null</code>)
-     * @throws UsernameNotFoundException if the user could not be found or the user has no
-     * GrantedAuthority
-     */
 
     @Resource
     private UserService userService;
@@ -39,6 +27,6 @@ public class CustomerUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("用户名错误或密码错误");
         }
 //        查询成功
-        return null;
+        return user;
     }
 }
