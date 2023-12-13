dcb06a0529f7f43df908f024a32836289d94b510
kimeray <kimerayforw@outlook.com>
2023-12-14 04:56:52 +0800

    feat: 用户授权

--- src/main/java/com/manong/dao/PermissionMapper.java
+++ src/main/java/com/manong/dao/PermissionMapper.java
@@ -3,7 +3,14 @@ package com.manong.dao;
 import com.manong.entity.Permission;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 
 public interface PermissionMapper extends BaseMapper<Permission> {
-
+    /**
+     * 根据用户ID查询权限列表
+     * @param userId
+     * @return
+     */
+    List<Permission> findPermissionListByUserId(Long userId);
 }
