139cc4d80b16124a35a028eae979b8eaebb83a10
kimeray <kimerayforw@outlook.com>
2023-12-31 00:15:14 +0800

    feat: 角色管理接口实现

--- src/main/java/com/manong/controller/RoleController.java
+++ src/main/java/com/manong/controller/RoleController.java
@@ -1,14 +1,94 @@
 package com.manong.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.manong.entity.Role;
+import com.manong.service.RoleService;
+import com.manong.utils.Result;
+import com.manong.vo.query.RoleQueryVo;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 
 @RestController
 @RequestMapping("/api/role")
 public class RoleController {
+    @Resource
+    private RoleService roleService;
 
+    @GetMapping("/list")
+    public Result list(RoleQueryVo roleQueryVo) {
+//    创建分页对象
+        IPage<Role> page = new Page<Role>
+                (roleQueryVo.getPageNo(), roleQueryVo.getPageSize());
+//    调用查询方法
+        roleService.findRoleListByUserId(page, roleQueryVo);
+//    返回数据
+        return Result.ok(page);
+    }
+
+    /**
+     * 添加角色
+     *
+     * @param role
+     * @return
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody Role role) {
+
+//        调用添加的方法
+        if (roleService.save(role)) {
+            return Result.ok().message("角色添加成功");
+        }
+        return Result.error().message("角色添加失败");
+    }
+
+    /**
+     * 修改角色
+     *
+     * @param role
+     * @return
+     */
+    @PutMapping("/update")
+    public Result update(@RequestBody Role role) {
+
+//        调用添加的方法
+        if (roleService.updateById(role)) {
+            return Result.ok().message("角色修改成功");
+        }
+        return Result.error().message("角色修改失败");
+    }
+
+    /**
+     * 删除角色
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/delete/{ijd}")
+    public Result delete(@PathVariable Long id) {
+
+//        调用删除的方法
+        if (roleService.deleteRoleById(id)) {
+            return Result.ok().message("角色删除成功");
+        }
+        return Result.error().message("角色删除失败");
+    }
+
+    /**
+     * 检查用户角色是否被使用
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/check/{id}")
+    public Result check(@PathVariable Long id) {
+//        调用检查用户角色是否被使用的方法
+        if (roleService.hasRoleCount(id)) {
+            return Result.exist().message("该角色已分配给其他用户使用，无法删除");
+        }
+        return Result.ok();
+    }
 }
 
--- src/main/java/com/manong/service/impl/RoleServiceImpl.java
+++ src/main/java/com/manong/service/impl/RoleServiceImpl.java
@@ -1,15 +1,77 @@
 package com.manong.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.manong.dao.UserMapper;
 import com.manong.entity.Role;
 import com.manong.dao.RoleMapper;
+import com.manong.entity.User;
 import com.manong.service.RoleService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.manong.vo.query.RoleQueryVo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
 
 
 @Service
 @Transactional
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements RoleService {
 
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 查询角色列表
+     *
+     * @param page
+     * @param roleQueryVo
+     * @return
+     */
+    @Override
+    public IPage<Role> findRoleListByUserId(IPage<Role> page, RoleQueryVo roleQueryVo) {
+//        创建条件构造器
+        QueryWrapper<Role> queryWrapper = new QueryWrapper<Role>();
+//        条件查询
+        queryWrapper.like(!ObjectUtils.isEmpty(roleQueryVo.getRoleName()), "role_name", roleQueryVo.getRoleName());
+//        排序
+        queryWrapper.orderByAsc("id");
+
+        //        根据用户ID查询用户信息
+        User user = userMapper.selectById(roleQueryVo.getUserId());
+//        判断对象是否为空
+        if (user != null && !ObjectUtils.isEmpty(user.getIsAdmin()) && user.getIsAdmin() != 1) {
+//            非管理员只能查询自己创建的角色
+            queryWrapper.eq("create_user", roleQueryVo.getUserId());
+        }
+        return baseMapper.selectPage(page, queryWrapper);
+
+    }
+
+    /**
+     * 检查角色是否被使用
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean hasRoleCount(Long id) {
+        return baseMapper.getRoleCountByRoleId(id) > 0;
+    }
+
+    /**
+     * 删除角色
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean deleteRoleById(Long id) {
+//        删除角色关系权限
+        baseMapper.deleteRolePermissiionByRoleId(id);
+//        删除角色
+        return baseMapper.deleteById(id) > 0;
+    }
 }
