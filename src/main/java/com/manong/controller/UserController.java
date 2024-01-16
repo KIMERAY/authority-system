cca4337aff6e2b3f669f6bb176e850a087111427
kimeray <kimerayforw@outlook.com>
2024-01-17 03:27:58 +0800

    feat: 查询用户列表后端接口

--- src/main/java/com/manong/controller/UserController.java
+++ src/main/java/com/manong/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.manong.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.manong.entity.User;
 import com.manong.service.UserService;
 import com.manong.utils.Result;
+import com.manong.vo.query.UserQueryVo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,6 +28,22 @@ public class UserController {
     @GetMapping("/listAll")
     public Result listAll() {
         return Result.ok(userService.list());
+    }
+
+
+    /**
+     * 查询用户列表
+     * @param userQueryVo
+     * @return
+     */
+    @GetMapping("/list")
+    public Result list(UserQueryVo userQueryVo){
+//        创建分页对象
+        IPage<User> page = new Page<User>(userQueryVo.getPageNo(), userQueryVo.getPageSize());
+//        调用分页查询方法
+        userService.findUserListByPage(page, userQueryVo);
+//        返回数据
+        return Result.ok(page);
     }
 }
 
